6b85cd75647b96da99c73462af29a00bd535ec54
RascalTwo <therealrascaltwo@gmail.com>
2015-10-29 14:14:46 -0400

    Increased performance of player & dealer card-counting.

--- src/com/github/captainrosco/BlackJack/Dealer.java
+++ src/com/github/captainrosco/BlackJack/Dealer.java
@@ -9,67 +9,69 @@ public class Dealer{
 	private ArrayList<Integer>	dealersHand;
 	private int					dealerTotal;
 	private int					playerTotal;
-	
+
 	public int draw_card(){
 		int card = random.nextInt(10 - 1 + 1) + 1;
 		return card;
 	}
-	
+
 	public void init(){
 		playersHand = new ArrayList<Integer>();
 		dealersHand = new ArrayList<Integer>();
 		playersHand.add(draw_card());
 		playersHand.add(draw_card());
+		for (int cards : playersHand){
+			playerTotal += cards;
+		}
 		dealersHand.add(draw_card());
 		dealersHand.add(draw_card());
+		for (int cards : dealersHand){
+			dealerTotal += cards;
+		}
 	}
-	
+
 	public void get_DealersHand(){
 		System.out.print("Dealers Hand: ");
 		for (int cards : dealersHand){
 			System.out.print(cards + " ");
 		}
-		
+
 	}
-	
+
 	public void get_PlayersHand(){
 		System.out.print("Your Hand: ");
 		for (int cards : playersHand){
 			System.out.print(cards + " ");
 		}
 	}
-	
+
 	public void playerHit(){
-		playersHand.add(draw_card());
+		int card = draw_card();
+		playersHand.add(card);
+		playerTotal += card;
 	}
-	
+
 	public void dealerHit(){
-		dealersHand.add(draw_card());
+		int card = draw_card();
+		dealersHand.add(card);
+		dealerTotal += card;
 	}
-	
+
 	public int playerTotal(){
-		playerTotal = 0;
-		for (int cards : playersHand){
-			playerTotal += cards;
-		}
 		return playerTotal;
 	}
-	
+
 	public int dealerTotal(){
-		dealerTotal = 0;
-		for (int cards : dealersHand){
-			dealerTotal += cards;
-		}
 		return dealerTotal;
 	}
-	
+
 	public boolean aceCheck(){
 		if (playersHand.contains(1)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public void aceConvert(){
 		if (playersHand.contains(1)) {
 			int index = playersHand.indexOf(1);
